b2aa4595b9cea16872da33ed4d05e7343fa91d06
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-05-13 17:04:44 -0300

    loops

---
+++ src/certification/controlflow/DoWhileLoop.java
@@ -0,0 +1,74 @@
+package certification.controlflow;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+        int a = 0;
+        do {
+            a++;
+        } while(false);
+
+        System.out.println("a = " + a);
+
+
+
+        //while (false) {
+        //a++;
+        //unreachable statement
+        //}
+
+        //while (true) {
+        //a++;
+        //endless loop
+        //}
+
+
+        //while e do-while sometimes have  the same behavior
+        int x = 20;
+        while(x > 10) x--;
+        System.out.println("x = " + x);
+
+        int x2 = 20;
+        do x2--;
+        while (x2 > 10);
+        System.out.println("x2 = " + x2);
+
+        int y = 10;
+        int z = 5;
+        while (y < 20)
+            y++;
+        // y = 20
+        z += 2;
+        // z = 7
+        y += 10;
+        // y = 30
+
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+
+        int number = 10;
+        while (number > 10) {
+            number--;
+        }
+
+        do{
+            number--;
+            //number = 9
+            while(number > 5){
+                number -= 2;
+                // number = 10
+            }
+        } while (number > 10);
+
+        System.out.println("number = " + number);
+
+        do{
+            number--;
+            //number = 9
+            while(number > 5){ //this is a infinite loop
+                number += 1;
+                // number = 10
+            }
+        } while (number < 10);
+    }
+}
---
+++ src/certification/controlflow/ForLoop.java
@@ -0,0 +1,44 @@
+package certification.controlflow;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+        }
+        // i can't see i here
+        // System.out.println("i = " + i);
+
+        System.out.println(); // prints new line sign
+
+        int a;
+        for(a = 0; a < 10; a++)
+            System.out.print(a + " ");
+
+        System.out.println();
+        System.out.println("After for loop a = " + a);
+
+        // infinite/endless for loop
+        for(;;) {
+            System.out.println("Hello!");
+            break;
+        }
+
+        int x = 0;
+        for(int y = 0, z = 4; x < 5 && y < 10; x++, y ++){
+            System.out.println("y = " + y);
+        }
+        System.out.println("x = " + x);
+
+        int d = 10;
+        //for(int e = 0, d = 0; e < 10 || d < 10; e++, d--)
+        // does not compile cause d variable is already declared
+
+        //for (long z = 0, int e = 4; z < 10; z++, e++)
+        // does not compile cause incompatible types
+
+        // for with different types
+        for (long z = 0; z < 10; z++){ }
+        for (double xy = 0.0; xy < 10.5; xy+=0.5) { }
+    }
+}
